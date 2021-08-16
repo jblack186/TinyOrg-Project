c884c7112a75ece734f9d742d4a6c2ca20897d2b
Jamison T Blackwell <blackwellj1040@gmail.com>
2021-08-15 23:26:27 -0400

    added link to new feature on header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,13 @@
 import React from "react";
 import '../css/Header.scss';
 import Logo from '../img/logo.png';
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header">
         <img className="header__logo" src={Logo} alt="logo" />
+    <Link style={{textDecoration: 'none', background: '#28a745', color:'white', padding: '8px', borderRadius: '8px'}} to='/compare'>Click here to see how we match up against other brands!</Link>
     </div>
   )
 }
